744233ef432934c1f26fa9ff2d61821cd29d1768
Marcelo Bracet <marcelo.bracet1@gmail.com>
2024-05-15 18:16:55 -0300

    refactor: Update image paths, implement lazy loading, and improve homepage layout and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-"use client";
+'use client';
 
-import { useEffect, useState } from "react";
-import Link from "next/link";
-import Image from "next/image";
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
-import mainData from "@/data/main-data";
-import { IMAGES } from "@/data";
+import mainData from '@/data/main-data';
+import { IMAGES } from '@/data';
 
-import { gsap } from "gsap";
+import { gsap } from 'gsap';
 
 const IMAGES_TIME_CHANGER = 7;
 
@@ -35,7 +35,7 @@ const { yellow, gray, lightAndDark, redAndOrange } = IMAGES.DESKTOP_IMAGES;
 export default function Home() {
   const [images, setImages] = useState(yellow);
   const [isMobile, setIsMobile] = useState(
-    window.innerWidth < 768 ? true : false
+    window.innerWidth < 768 ? true : false,
   );
   const [idx, setIdx] = useState(0);
 
@@ -44,9 +44,9 @@ export default function Home() {
       setIsMobile(window.innerWidth < 768);
     };
 
-    window.addEventListener("resize", handleResize);
+    window.addEventListener('resize', handleResize);
 
-    return () => window.removeEventListener("resize", handleResize);
+    return () => window.removeEventListener('resize', handleResize);
   }, [isMobile, setIsMobile]);
 
   useEffect(() => {
@@ -58,7 +58,7 @@ export default function Home() {
 
         // Realize a animação de fade
         gsap.fromTo(
-          ".mobile-img",
+          '.mobile-img',
           {
             opacity: 0, // Comece com opacidade 0
           },
@@ -68,13 +68,31 @@ export default function Home() {
             onComplete: () => {
               setImages(nextImages);
             },
-          }
+          },
+        );
+
+        gsap.fromTo(
+          '.first-img-desktop',
+          { opacity: 0 },
+          {
+            duration: 1.3,
+            opacity: 1,
+            onComplete: () => {
+              setImages(nextImages);
+            },
+          },
+        );
+
+        gsap.fromTo(
+          '.second-img-desktop',
+          { opacity: 0 },
+          { duration: 1.3, opacity: 1 }, // Adiciona um pequeno atraso para suavidade
         );
 
         return nextImages;
       });
       setIdx((prevIdx) =>
-        prevIdx + 1 === IMAGES.MOBILE_IMAGES.length ? 0 : prevIdx + 1
+        prevIdx + 1 === IMAGES.MOBILE_IMAGES.length ? 0 : prevIdx + 1,
       );
     }, IMAGES_TIME_CHANGER * 1000);
 
@@ -93,8 +111,8 @@ export default function Home() {
           <div className="flex-1 relative h-screen">
             <Image
               className={`h-full ${
-                imageOne === "/assets/gray.jpeg" ? "bg-bottom" : "bg-center"
-              } object-cover bg-cover brightness-50`}
+                imageOne === '/assets/gray.jpeg' ? 'bg-bottom' : 'bg-center'
+              } object-cover bg-cover brightness-50 first-img-desktop`}
               loading="lazy"
               src={imageOne}
               layout="fill"
@@ -103,7 +121,7 @@ export default function Home() {
           </div>
           <div className="flex-1 relative h-screen">
             <Image
-              className="h-full brightness-50 object-cover bg-center bg-cover"
+              className="h-full brightness-50 object-cover bg-center second-img-desktop bg-cover"
               loading="lazy"
               layout="fill"
               src={imageTwo}
@@ -116,7 +134,7 @@ export default function Home() {
       {isMobile && (
         <div className="relative h-screen w-full">
           <Image
-            className="h-full object-cover bg-center bg-cover brightness-50 mobile-img "
+            className="h-full object-cover bg-center  bg-cover brightness-50 mobile-img "
             loading="eager"
             layout="fill"
             src={IMAGES.MOBILE_IMAGES[idx].src}
@@ -142,7 +160,7 @@ export default function Home() {
             <SocialMediaLinks />
           </div>
           <div className="flex gap-3 mt-5">
-            <p>{mainData.address.street}</p> |{" "}
+            <p>{mainData.address.street}</p> |{' '}
             <p>{mainData.address.neighborhood}</p>
           </div>
         </div>
